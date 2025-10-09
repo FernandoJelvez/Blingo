0d80d0cc1f78e330614f426a179771719b70c0c2
FernandoJelvez <f.jelvez02@ufromail.cl>
2025-10-09 16:53:57 -0300

    Created the base files for the project and erased the previous structure.

---
+++ src/main/java/com/blingo/lingdyo/LingdyoApplication.java
@@ -0,0 +1,13 @@
+package com.blingo.lingdyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingdyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingdyoApplication.class, args);
+	}
+
+}
