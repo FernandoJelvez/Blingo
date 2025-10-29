a72b68efabc3aca8727a6d529695b585afacc076
FernandoJelvez <f.jelvez02@ufromail.cl>
2025-10-29 18:36:57 -0300

    Modified the sidebar +Added a grid to the sidebar +Added a header to the sidebar +Added a board to the sidebar (planned to be used to show messages, advice or news) +Added a profile card at the bottom of the sidebar, redirects to the login page +Added the css and images folder to the folders allowed without authentication +Added a "no user" icon (obtained from wikimedia commons)

--- src/main/java/com/blingo/lingdyo/configuration/WebSecurityConfig.java
+++ src/main/java/com/blingo/lingdyo/configuration/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests)-> requests
-                        .requestMatchers("/","/about").permitAll()
+                        .requestMatchers("/","/about","/images/**","/css/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form)->form
