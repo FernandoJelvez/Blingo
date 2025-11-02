55430f7248c2840eb8472712619ec70af0f3085e
FernandoJelvez <f.jelvez02@ufromail.cl>
2025-11-01 23:27:01 -0300

    User's Home now shows the username and the user button (login button in the index page) leads to "my/profile"

---
+++ src/main/java/com/blingo/lingdyo/controllers/MyHomeController.java
@@ -0,0 +1,17 @@
+package com.blingo.lingdyo.controllers;
+
+import com.blingo.lingdyo.configuration.CustomUserDetails;
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class MyHomeController {
+    @GetMapping("/my/home")
+    public String myHome(Model model, @AuthenticationPrincipal CustomUserDetails userDetails){
+        model.addAttribute("username",userDetails.getUsername());
+        return "my/home";
+    }
+}
