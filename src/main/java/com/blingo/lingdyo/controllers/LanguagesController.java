af2a017b88b0aa9bb8a97033e47992e090967773
FernandoJelvez <f.jelvez02@ufromail.cl>
2025-12-03 15:00:31 -0300

    fix: added autowired repositories to some controllers in order to fix null repositories

--- src/main/java/com/blingo/lingdyo/controllers/LanguagesController.java
+++ src/main/java/com/blingo/lingdyo/controllers/LanguagesController.java
@@ -2,14 +2,16 @@ package com.blingo.lingdyo.controllers;
 
 import com.blingo.lingdyo.dtos.LanguageDto;
 import com.blingo.lingdyo.services.LanguagesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguagesController {
+    @Autowired
+    LanguagesService languagesService;
     @GetMapping("/languages")
     public LanguageDto[] languages(){
-        LanguagesService langServ = new LanguagesService();
-        return langServ.getLanguages();
+        return languagesService.getLanguages();
     }
 }
