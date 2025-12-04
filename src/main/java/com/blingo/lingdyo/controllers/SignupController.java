10d37128f97e81cae9d1953bbacdcc8dd6be1884
DiegoT <d.torres19@ufromail.cl>
2025-12-04 04:59:17 -0300

    Added logs for SignupController.

--- src/main/java/com/blingo/lingdyo/controllers/SignupController.java
+++ src/main/java/com/blingo/lingdyo/controllers/SignupController.java
@@ -1,8 +1,10 @@
 package com.blingo.lingdyo.controllers;
 
 import com.blingo.lingdyo.User;
+import com.blingo.lingdyo.repositories.UserRepository;
 import com.blingo.lingdyo.services.UserService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j(topic = "customLogs")
 @Controller
 public class SignupController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/signup")
     public String signup(Model model) {
         model.addAttribute("user", new User());
@@ -24,9 +30,13 @@ public class SignupController {
     public ModelAndView signup(@ModelAttribute("user") @Valid User user) {
         try {
             userService.registerNewUserAccount(user);
+            if(userRepository.existsByUsername(user.getUsername())){
+                log.warn("controllers.SignupController - The user "+user.getUsername()+" has been registered");
+            }
         } catch (Exception e) {
             ModelAndView mav = new ModelAndView("signup");
             mav.addObject("message", "An error has occurred, please contact an administrator");
+            log.error("controllers.SignupController - Error registering user - "+e);
             return mav;
         }
 
