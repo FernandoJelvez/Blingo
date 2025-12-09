8217302fd2c3201c082cb19058855ec5ece84620
DiegoT <d.torres19@ufromail.cl>
2025-12-08 23:59:15 -0300

    Added error log for CustomUserDetailsService

--- src/main/java/com/blingo/lingdyo/services/CustomUserDetailsService.java
+++ src/main/java/com/blingo/lingdyo/services/CustomUserDetailsService.java
@@ -18,16 +18,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> {
                     log.error("services.CustomUserDetailsService - ERROR: User '{}' not found during login attempt.",
                             username);
                     return new UsernameNotFoundException("Usuario " + username + " no encontrado");
                 });
-
-        log.info("services.CustomUserDetailsService - User '{}' found. Password hash loaded.", username);;
-
         return new CustomUserDetails(user);
     }
 }
