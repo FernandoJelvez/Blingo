6999bb558b12bafd6f477f569eb1818475991203
DiegoT <d.torres19@ufromail.cl>
2025-12-08 23:58:09 -0300

    Added error log for CustomUserDetailsService

--- src/main/java/com/blingo/lingdyo/services/CustomUserDetailsService.java
+++ src/main/java/com/blingo/lingdyo/services/CustomUserDetailsService.java
@@ -3,10 +3,12 @@ package com.blingo.lingdyo.services;
 import com.blingo.lingdyo.CustomUserDetails;
 import com.blingo.lingdyo.User;
 import com.blingo.lingdyo.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j(topic = "customLogs")
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
@@ -18,11 +20,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() ->
-                        new UsernameNotFoundException("Usuario " + username + " no encontrado")
-                );
+                .orElseThrow(() -> {
+                    log.error("services.CustomUserDetailsService - ERROR: User '{}' not found during login attempt.",
+                            username);
+                    return new UsernameNotFoundException("Usuario " + username + " no encontrado");
+                });
 
-        System.out.println("Usuario " + username + " detectado, Hash de contraseña almacenado.");
+        log.info("services.CustomUserDetailsService - User '{}' found. Password hash loaded.", username);;
 
         return new CustomUserDetails(user);
     }
